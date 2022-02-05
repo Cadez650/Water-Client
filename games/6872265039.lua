5c56e1c30bf3f940821a4d9906d795b5c24e0c36
Cadez650 <firemancade@outlook.com>
2022-02-04 19:39:59 -0800

    d
    d

--- games/6872265039.lua
+++ games/6872265039.lua
@@ -1,1 +1,6 @@
-print("Authenticated to Bedwars.")
+local guiLibrary = loadstring(game:HttpGet("https://pastebin.com/raw/SKZjdjdj"))()
+
+local Section = guiLibrary:CreateSection("Test")
+Section:Button("Infinite Yield FE", function()
+    print("Pressed!")
+end)
